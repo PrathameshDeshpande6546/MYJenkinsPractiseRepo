115535762f85f1cd823eaf29081a3f23c4947cb4
Prathamesh <prathameshdeshpande68@gmail.com>
2024-11-28 17:27:19 +0530

    BGCOLOR

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{textAlign:'center'}}>
+    <div className="App" style={{textAlign:'center',backgroundColor:'red'}}>
    <h1>
     Prathamesh Deshpande
    </h1>
